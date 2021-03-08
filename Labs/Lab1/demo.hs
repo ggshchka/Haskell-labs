61b8025f9654cbbac04b3ff1c18756fbeb23c64c
ggshchka <ch.us@mail.ru>
2021-03-08 11:17:34 +0300

    ref

--- Labs/Lab1/demo.hs
+++ Labs/Lab1/demo.hs
@@ -3,13 +3,10 @@ import qualified Data.ByteString.Char8 as C
 
 pascal :: Int -> Int -> Int
 pascal c r 
-    | c == 0 = 1  
-    | c > r = 0  
+    | (c < 0) || (r < 0) = error "args should be a positive" --error "column should be greater than row"  
+    | c == 0 = 1
+    | c > r = 0
     | otherwise = (pascal c (r - 1)) + (pascal (c - 1) (r - 1))
-
-printRows :: Int -> IO ()
-printRows n = mapM_ print [[pascal y x | y <- [0..x]] | x <- [0..n]]
-
 
 printIt :: Int -> C.ByteString
 printIt n = C.pack $ show $ [pascal y x | x <- [0..n], y <- [0..x]]
@@ -18,7 +15,7 @@ printItIo :: Int -> IO ()
 printItIo n = mapM_ print [[pascal y x | y <- [0..x]] | x <- [0..n]]
 
 
-main = printItIo 6
+main = printItIo 12
 
 
 {- 
